838177f5e160cf940083a08b9ce70633cac94a8a
ee7 <45465154+ee7@users.noreply.github.com>
2024-05-04 15:00:01 +0200

    .github, install_nim: bump Nim from 2.0.2 to 2.0.4
    See the release post [1] and changes [2].
    
    [1] https://nim-lang.org/blog/2024/04/16/versions-1620-204-released.html
    [2] https://github.com/nim-lang/Nim/compare/v2.0.2...v2.0.4

--- bin/install_nim.sh
+++ bin/install_nim.sh
@@ -3,7 +3,7 @@
 set -ex
 
 readonly ARCHIVE_FILENAME='nim.tar.xz'
-readonly NIM_VERSION='2.0.2'
+readonly NIM_VERSION='2.0.4'
 readonly BUILD_DIR='/build/nim'
 readonly INSTALL_DIR='/nim/'
 mkdir -p "${BUILD_DIR}"
